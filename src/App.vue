0cab1de51a63962a34bce77aab1750a38dfd09bb
Alpha <46735230+alphatero@users.noreply.github.com>
2022-03-07 13:55:57 +0800

    vue composition api last version ref issue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref, onMounted } from "@vue/runtime-core";
+import HelloWorld from "./components/HelloWorld.vue";
+const InputRef1 = ref(null);
+const InputRef2 = ref(null);
+const InputRef3 = ref(null);
+const InputRef4 = ref(null);
+onMounted(() => console.log(InputRef1.value));
+</script>
+
+<template>
+  <div class="text-center selection:bg-green-100">
+    <img class="mx-auto mt-12 mb-8" alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld
+      v-for="index in 4"
+      :key="index"
+      :ref="`InputRef${index}`"
+      msg="Vite + Vue 3 + Tailwindcss v3"
+    />
+    <img
+      class="mx-auto my-8"
+      alt="powered-by"
+      src="./assets/powered-by-vitawind-bright.png"
+    />
+  </div>
+</template>
+
+<style></style>
